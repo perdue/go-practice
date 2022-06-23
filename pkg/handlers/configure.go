e5ed957c64698d88bf2749927f6bd4fde768aa15
perdue <perdue@users.noreply.github.com>
2022-06-23 11:45:38 -0400

    Stub out handlers

--- pkg/handlers/configure.go
+++ pkg/handlers/configure.go
@@ -12,22 +12,22 @@ func ConfigureHandlers(handler Handler) *mux.Router {
 	router.Methods("GET").Path("/").Handler(http.HandlerFunc(handler.Index))
 
 	// TODO: Implement ProductIndex
-	router.Methods("GET").Path("/products").Handler(http.HandlerFunc(notImplemented))
+	router.Methods("GET").Path("/products").Handler(http.HandlerFunc(handler.ProductIndex))
 
 	// TODO: Implement OrderShow
-	router.Methods("GET").Path("/orders/{orderId}").Handler(http.HandlerFunc(notImplemented))
+	router.Methods("GET").Path("/orders/{orderId}").Handler(http.HandlerFunc(handler.OrderShow))
 
 	// TODO: Implement OrderInsert
-	router.Methods("POST").Path("/orders").Handler(http.HandlerFunc(notImplemented))
+	router.Methods("POST").Path("/orders").Handler(http.HandlerFunc(handler.OrderInsert))
 
 	// TODO: Implement Close
-	router.Methods("POST").Path("/close").Handler(http.HandlerFunc(notImplemented))
+	router.Methods("POST").Path("/close").Handler(http.HandlerFunc(handler.Close))
 
 	// TODO: Implement Stats
-	router.Methods("GET").Path("/stats").Handler(http.HandlerFunc(notImplemented))
+	router.Methods("GET").Path("/stats").Handler(http.HandlerFunc(handler.Stats))
 
 	// TODO: Implement OrderReverse
-	router.Methods("DELETE").Path("/orders/{orderId}").Handler(http.HandlerFunc(notImplemented))
+	router.Methods("DELETE").Path("/orders/{orderId}").Handler(http.HandlerFunc(handler.OrderReverse))
 
 	return router
 }
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -15,6 +15,12 @@ type handler struct {
 
 type Handler interface {
 	Index(w http.ResponseWriter, r *http.Request)
+	ProductIndex(w http.ResponseWriter, r *http.Request)
+	OrderShow(w http.ResponseWriter, r *http.Request)
+	OrderInsert(w http.ResponseWriter, r *http.Request)
+	Close(w http.ResponseWriter, r *http.Request)
+	Stats(w http.ResponseWriter, r *http.Request)
+	OrderReverse(w http.ResponseWriter, r *http.Request)
 }
 
 func New() (Handler, error) {
@@ -26,6 +32,36 @@ func New() (Handler, error) {
 // Index returns a simple welcome response for the home page
 func (h *handler) Index(w http.ResponseWriter, r *http.Request) {
 	writeResponse(w, http.StatusOK, "Welcome to the Orders App!", nil)
+}
+
+// ProductIndex displays all the products in the system
+func (h *handler) ProductIndex(w http.ResponseWriter, r *http.Request) {
+	writeResponse(w, http.StatusOK, "Welcome to the Product Index", nil)
+}
+
+// OrderShow
+func (h *handler) OrderShow(w http.ResponseWriter, r *http.Request) {
+	writeResponse(w, http.StatusOK, "Welcome to the Order Show", nil)
+}
+
+// OrderInsert
+func (h *handler) OrderInsert(w http.ResponseWriter, r *http.Request) {
+	writeResponse(w, http.StatusOK, "Welcome to the Order Insert", nil)
+}
+
+// Close
+func (h *handler) Close(w http.ResponseWriter, r *http.Request) {
+	writeResponse(w, http.StatusOK, "Welcome to the Close", nil)
+}
+
+// Stats
+func (h *handler) Stats(w http.ResponseWriter, r *http.Request) {
+	writeResponse(w, http.StatusOK, "Welcome to the Stats", nil)
+}
+
+// OrderReverse
+func (h *handler) OrderReverse(w http.ResponseWriter, r *http.Request) {
+	writeResponse(w, http.StatusOK, "Welcome to the Order Reverse", nil)
 }
 
 func logMsg(msg string) {
